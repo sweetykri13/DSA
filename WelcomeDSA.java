587324e0ad6cff3bd61e698a233d2b95a13df282
Sweety Kumari <45881987+sweetykri13@users.noreply.github.com>
2025-04-07 11:55:20 +0530

    Add files via upload

---
+++ WelcomeDSA.java
@@ -0,0 +1,10 @@
+package Day_01;
+
+public class WelcomeDSA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from Day 01!");
+	}
+
+}
